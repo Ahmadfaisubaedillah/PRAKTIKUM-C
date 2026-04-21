f08f1da209c896204dec2801ae051fab0a5e6241
Ahmadfaisubaedillah <faisubaidillahahmadfais@gmail.com>
2026-04-21 23:04:17 +0700

    Format input.c and remove unnecessary comments

--- input.c
+++ input.c
@@ -4,14 +4,13 @@ int main() {
     char nama[50];
     int nilai;
 
-    // Input
+
     printf("Masukkan nama mahasiswa: ");
     fgets(nama, sizeof(nama), stdin);
 
     printf("Masukkan nilai: ");
     scanf("%d", &nilai);
 
-    // Output
     printf("\nNama: %s", nama);
     printf("Nilai: %d\n", nilai);
 
